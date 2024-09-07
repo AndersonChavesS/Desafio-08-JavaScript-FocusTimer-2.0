ace58bdedc7ec027d51a8a2236f10ded1558bc67
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2024-09-07 12:49:45 -0300

    feat: first play and stop actions configured

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+const play = document.querySelector('.play');
+const stop = document.querySelector('.stop');
+const controls = document.getElementById('controls');
 // toggleMode
 let lightMode = true;
 const buttonToggle = document.getElementById('toggle-mode');
@@ -7,3 +10,22 @@ buttonToggle.addEventListener('click', (e) => {
   e.currentTarget.querySelector('span').textContent = `${mode} mode ativado`;
   lightMode = !lightMode;
 });
+
+// functions
+function counterInit(e) {
+  play.classList.toggle('active');
+
+  
+}
+
+function counterStop(e) {
+  if (play.classList.contains('active')) {
+    play.classList.remove('active');
+  } else {
+    return;
+  }
+}
+// events
+play.addEventListener('click', counterInit);
+
+stop.addEventListener('click', counterStop);
